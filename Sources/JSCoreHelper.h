7262b9b6c440876d2165fa808599988e267d0cc7
jiasong <593908937@qq.com>
2021-01-07 14:28:45 +0800

    适配

--- Sources/JSCoreHelper.h
+++ Sources/JSCoreHelper.h
@@ -29,42 +29,91 @@ typedef NS_ENUM(NSInteger, JSCoreAnimationExtrapolateType) {
 @interface JSCoreHelper (UIGraphic)
 
 /// 获取一像素的大小
-+ (CGFloat)pixelOne;
+@property (class, nonatomic, readonly) CGFloat pixelOne;
 
 @end
 
 @interface JSCoreHelper (Device)
 
-+ (BOOL)isMac;
-+ (BOOL)isIPad;
-+ (BOOL)isIPod;
-+ (BOOL)isIPhone;
-+ (BOOL)isSimulator;
+/// 如 iPhone12,5、iPad6,8
+/// @NEW_DEVICE_CHECKER
+@property (class, nonatomic, readonly) NSString *deviceModel;
+
+/// 如 iPhone 11 Pro Max、iPad Pro (12.9 inch)，如果是模拟器，会在后面带上“ Simulator”字样。
+/// @NEW_DEVICE_CHECKER
+@property (class, nonatomic, readonly) NSString *deviceName;
+
+@property (class, nonatomic, readonly) BOOL isMac;
+@property (class, nonatomic, readonly) BOOL isIPad;
+@property (class, nonatomic, readonly) BOOL isIPod;
+@property (class, nonatomic, readonly) BOOL isIPhone;
+@property (class, nonatomic, readonly) BOOL isSimulator;
 
 /// 带物理凹槽的刘海屏或者使用 Home Indicator 类型的设备
-+ (BOOL)isNotchedScreen;
+@property (class, nonatomic, readonly) BOOL isNotchedScreen;
 
-+ (CGSize)screenSizeFor65Inch;
-+ (CGSize)screenSizeFor61Inch;
-+ (CGSize)screenSizeFor58Inch;
-+ (CGSize)screenSizeFor55Inch;
-+ (CGSize)screenSizeFor47Inch;
-+ (CGSize)screenSizeFor40Inch;
-+ (CGSize)screenSizeFor35Inch;
+/// iPhone 12 Pro Max
+@property (class, nonatomic, readonly) BOOL is67InchScreen;
 
-// 用于获取 isNotchedScreen 设备的 insets，注意对于 iPad Pro 11-inch 这种无刘海凹槽但却有使用 Home Indicator 的设备，它的 top 返回0，bottom 返回 safeAreaInsets.bottom 的值
-+ (UIEdgeInsets)safeAreaInsetsForDeviceWithNotch;
+/// iPhone XS Max / 11 Pro Max
+@property (class, nonatomic, readonly) BOOL is65InchScreen;
 
-// 状态栏动态高度
-+ (CGFloat)statusBarHeight;
-// 状态栏静态高度
-+ (CGFloat)statusBarHeightConstant;
+/// iPhone 12 / 12 Pro
+@property (class, nonatomic, readonly) BOOL is61InchScreenAndiPhone12;
 
-/**
- 在 iPad 分屏模式下可获得实际运行区域的窗口大小，如需适配 iPad 分屏，建议用这个方法来代替 [UIScreen mainScreen].bounds.size
- @return 应用运行的窗口大小
- */
-+ (CGSize)applicationSize;
+/// iPhone XR / 11
+@property (class, nonatomic, readonly) BOOL is61InchScreen;
+
+/// iPhone X / XS / 11Pro
+@property (class, nonatomic, readonly) BOOL is58InchScreen;
+
+/// iPhone 8 Plus
+@property (class, nonatomic, readonly) BOOL is55InchScreen;
+
+/// iPhone 12 mini
+@property (class, nonatomic, readonly) BOOL is54InchScreen;
+
+/// iPhone 8
+@property (class, nonatomic, readonly) BOOL is47InchScreen;
+
+/// iPhone 5
+@property (class, nonatomic, readonly) BOOL is40InchScreen;
+
+/// iPhone 4
+@property (class, nonatomic, readonly) BOOL is35InchScreen;
+
+@property (class, nonatomic, readonly) CGSize screenSizeFor67Inch;
+@property (class, nonatomic, readonly) CGSize screenSizeFor65Inch;
+@property (class, nonatomic, readonly) CGSize screenSizeFor61InchAndiPhone12;
+@property (class, nonatomic, readonly) CGSize screenSizeFor61Inch;
+@property (class, nonatomic, readonly) CGSize screenSizeFor58Inch;
+@property (class, nonatomic, readonly) CGSize screenSizeFor55Inch;
+@property (class, nonatomic, readonly) CGSize screenSizeFor54Inch;
+@property (class, nonatomic, readonly) CGSize screenSizeFor47Inch;
+@property (class, nonatomic, readonly) CGSize screenSizeFor40Inch;
+@property (class, nonatomic, readonly) CGSize screenSizeFor35Inch;
+
+/// 用于获取 isNotchedScreen 设备的 insets，注意对于 iPad Pro 11-inch 这种无刘海凹槽但却有使用 Home Indicator 的设备，它的 top 返回0，bottom 返回 safeAreaInsets.bottom 的值
+/// @NEW_DEVICE_CHECKER
+@property (class, nonatomic, readonly) UIEdgeInsets safeAreaInsetsForDeviceWithNotch;
+
+/// 系统设置里是否开启了“放大显示-试图-放大”，支持放大模式的 iPhone 设备可在官方文档中查询 https://support.apple.com/zh-cn/guide/iphone/iphd6804774e/ios
+/// @NEW_DEVICE_CHECKER
+@property (class, nonatomic, readonly) BOOL isZoomedMode;
+
+/// 状态栏动态高度
+@property (class, nonatomic, readonly) CGFloat statusBarHeight;
+/// 状态栏静态高度
+@property (class, nonatomic, readonly) CGFloat statusBarHeightConstant;
+
+/// 屏幕尺寸，会根据横竖屏的变化而变化
+@property (class, nonatomic, readonly) CGSize screenSize;
+
+/// 设备尺寸，跟横竖屏无关
+@property (class, nonatomic, readonly) CGSize deviceSize;
+
+/// 在 iPad 分屏模式下可获得实际运行区域的窗口大小，如需适配 iPad 分屏，建议用这个方法来代替 [UIScreen mainScreen].bounds.size
+@property (class, nonatomic, readonly) CGSize applicationSize;
 
 @end
 
